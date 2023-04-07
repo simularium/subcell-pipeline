3ce1e0ce5ed2e4986a21e267315a248750f40abb
Blair Lyons <blair208@gmail.com>
2023-04-06 18:13:46 -0700

    readdy loading and post processing for linear fiber points

---
+++ subcell_analysis/bin/readdy_loading_example.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+
+from subcell_analysis.postprocessing.readdy import (
+    ReaddyLoader, 
+    ReaddyPostProcessor,
+)
+
+
+class Args(argparse.Namespace):
+    def __init__(self) -> None:
+        self.__parse()
+
+    def __parse(self) -> None:
+        p = argparse.ArgumentParser(
+            prog="readdy-actin-fiber-points",
+            description=(
+                "Load a ReaDDy actin trajectory and "
+                "calculate actin fiber control points."
+            ),
+        )
+        p.add_argument(
+            "h5_file_path",
+            type=str,
+            help="The path to the ReaDDy .h5 file",
+        )
+        p.parse_args(namespace=self)
+
+def main() -> None:
+    args = Args()
+    fiber_points = ReaddyPostProcessor(
+        ReaddyLoader(args.h5_file_path).trajectory(), 
+        box_size=600. * np.ones(3),
+    ).linear_fiber_points(
+        start_particle_phrases=["pointed"],
+        other_particle_types=[
+            "actin#",
+            "actin#ATP_",
+            "actin#mid_",
+            "actin#mid_ATP_",
+            "actin#fixed_",
+            "actin#fixed_ATP_",
+            "actin#mid_fixed_",
+            "actin#mid_fixed_ATP_",
+            "actin#barbed_",
+            "actin#barbed_ATP_",
+            "actin#fixed_barbed_",
+            "actin#fixed_barbed_ATP_",
+        ],
+        polymer_number_range=5,
+        ideal_positions=np.array([
+            [24.738, 20.881, 26.671],
+            [27.609, 24.061, 27.598],
+            [30.382, 21.190, 25.725],
+        ]),
+        ideal_vector_to_axis=np.array(
+            [-0.01056751, -1.47785105, -0.65833209],
+        ),
+        segment_length=10.,
+    )
+    print(fiber_points)
+
+if __name__ == "__main__":
+    main()
---
+++ subcell_analysis/postprocessing/readdy/readdy_data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import Dict, List
+
+import numpy as np
+
+
+class TopologyData:
+    uid: int
+    type_name: str
+    particle_ids: List[int]
+    
+    def __init__(self, uid: int, type_name: str, particle_ids: List[int]):
+        """
+        Data class representing a ReaDDy topology of connected particles.
+        """
+        self.uid = uid
+        self.type_name = type_name
+        self.particle_ids = particle_ids
+
+
+class ParticleData:
+    uid: int
+    type_name: str
+    position: np.ndarray
+    neighbor_ids: List[int]
+    
+    def __init__(self, uid: int, type_name: str, position: np.ndarray, neighbor_ids: List[int]):
+        """
+        Data class representing a ReaDDy particle.
+        """
+        self.uid = uid
+        self.type_name = type_name
+        self.position = position
+        self.neighbor_ids = neighbor_ids
+
+
+class FrameData:
+    time: float
+    topologies: Dict[int, TopologyData]
+    particles: Dict[int, ParticleData]
+    
+    def __init__(
+        self, 
+        time: float, 
+        topologies: Dict[int, TopologyData] = None, 
+        particles: Dict[int, ParticleData] = None
+    ):
+        """
+        Data class representing one ReaDDy timestep.
+        """
+        self.time = time
+        self.topologies = topologies if topologies is not None else {}
+        self.particles = particles if particles is not None else {}
