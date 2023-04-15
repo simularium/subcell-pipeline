df620b0f089ab4ade24047046f65c3458441f3b0
Blair Lyons <blair208@gmail.com>
2023-04-14 19:06:22 -0700

    fix lint and test errors

--- subcell_analysis/readdy/__init__.py
+++ subcell_analysis/readdy/__init__.py
@@ -1,2 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""readdy package for subcell_analysis."""
--- subcell_analysis/tests/test_example.py
+++ subcell_analysis/tests/test_example.py
@@ -1,18 +1,16 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 
-# import pytest
-
-# from subcell_analysis import example
+import pytest
 
 
-# @pytest.mark.parametrize(
-#     "string, count",
-#     [
-#         ("string", 6),
-#         ("hello", 5),
-#         ("world", 5),
-#         ("defenestration", 14),
-#     ],
-# )
-# def test_str_len(string: str, count: int) -> None:
-#     assert example.str_len(string) == count
+@pytest.mark.parametrize(
+    "string, count",
+    [
+        ("string", 6),
+        ("hello", 5),
+        ("world", 5),
+        ("defenestration", 14),
+    ],
+)
+def test_str_len(string: str, count: int) -> None:
+    assert len(string) == count
