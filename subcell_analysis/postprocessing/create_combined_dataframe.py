0f2344031d5239494b3a9e4260c2a7c33ca3f063
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2024-04-17 13:14:49 -0700

    Refactor data loading and saving in create_combined_dataframe.py

--- subcell_analysis/postprocessing/create_combined_dataframe.py
+++ subcell_analysis/postprocessing/create_combined_dataframe.py
@@ -21,20 +21,10 @@ readdy_df_path = base_df_path / "readdy"
 print(base_df_path)
 
 # %%
-cytosim_df = pd.read_csv(
-    cytosim_df_path / "cytosim_actin_compression_all_velocities_and_repeats.csv"
-)
-readdy_df = pd.read_csv(
-    readdy_df_path / "readdy_actin_compression_all_velocities_and_repeats.csv"
-)
-# %% convert columns to compressible types
-cytosim_df = cytosim_df.convert_dtypes()
-readdy_df = readdy_df.convert_dtypes()
-# %% save df as parquet
-cytosim_df.to_parquet(
+cytosim_df = pd.read_parquet(
     cytosim_df_path / "cytosim_actin_compression_all_velocities_and_repeats.parquet"
 )
-readdy_df.to_parquet(
+readdy_df = pd.read_parquet(
     readdy_df_path / "readdy_actin_compression_all_velocities_and_repeats.parquet"
 )
 # %% merge dataframes
@@ -42,7 +32,6 @@ combined_df = pd.concat([cytosim_df, readdy_df], ignore_index=True)
 unnamed_cols = [col for col in combined_df.columns if "Unnamed" in col]
 combined_df = combined_df.drop(columns=unnamed_cols)
 combined_df = combined_df.drop(columns=["id"])
-combined_df = combined_df.convert_dtypes()
 # %% save as csv and parquet
 combined_df.to_csv(
     base_df_path / "combined_actin_compression_dataset_all_velocities_and_repeats.csv"
@@ -52,20 +41,10 @@ combined_df.to_parquet(
     / "combined_actin_compression_dataset_all_velocities_and_repeats.parquet"
 )
 # %% load subsampled dataframes
-subsampled_cytosim_df = pd.read_csv(
-    cytosim_df_path / "cytosim_actin_compression_subsampled.csv"
-)
-subsampled_readdy_df = pd.read_csv(
-    readdy_df_path / "readdy_actin_compression_subsampled.csv"
-)
-# %% convert columns to compressible types
-subsampled_cytosim_df = subsampled_cytosim_df.convert_dtypes()
-subsampled_readdy_df = subsampled_readdy_df.convert_dtypes()
-# %% save df as parquet
-subsampled_cytosim_df.to_parquet(
+subsampled_cytosim_df = pd.read_parquet(
     cytosim_df_path / "cytosim_actin_compression_subsampled.parquet"
 )
-subsampled_readdy_df.to_parquet(
+subsampled_readdy_df = pd.read_parquet(
     readdy_df_path / "readdy_actin_compression_subsampled.parquet"
 )
 # %% combine subsampled dataframes
@@ -74,7 +53,6 @@ subsampled_combined_df = pd.concat(
 )
 unnamed_cols = [col for col in subsampled_combined_df.columns if "Unnamed" in col]
 subsampled_combined_df = subsampled_combined_df.drop(columns=unnamed_cols)
-subsampled_combined_df = subsampled_combined_df.convert_dtypes()
 # %% save as csv and parquet
 subsampled_combined_df.to_csv(
     base_df_path
